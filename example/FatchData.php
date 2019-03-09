367ee5bc29c41550d330cf31ea155dad7d2142a0
suchart bunhachirat <suchartbu@gmail.com>
2019-03-09 14:01:09 +0700

    Initial Jdo.php

--- example/FatchData.php
+++ example/FatchData.php
@@ -1,6 +1,19 @@
 <?php
+
+/**
+ * Testing fatch data from AS400 DB2 with JDBC.
+ * 
+ * PHP Version 7
+ * 
+ * @category  Jdo
+ * @package   Example
+ * @author    suchart bunhachirat <suchartbu@gmail.com>
+ * @copyright 2019 Suchart Bunhachirat
+ */
 require_once '../src/Jdo.php';
+
 use Orr\Jdo as Jdo;
+
 $sql = "SELECT * FROM jdbc_test WHERE id > 0";
 $user = "orrconn";
 $passwd = "xoylfk";
